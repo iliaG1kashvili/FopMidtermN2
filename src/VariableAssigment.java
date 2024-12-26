ef0a5504fff219851d39d1d8f7312898b959848b
nikoloz261 <nikushajvebenava2@gmail.com>
2024-12-27 02:16:04 +0400

    added first 4 algorithms that our interpreter should translate to java

--- src/VariableAssigment.java
+++ src/VariableAssigment.java
@@ -2,4 +2,68 @@ import java.util.Scanner;
 
 public class VariableAssigment {
     Scanner scanner = new Scanner(System.in);
+
+//1)
+//    # function for sum of first n numbers #
+//    def sum_N_numbers(n)
+//#firstly we are checking if n is non positive, if it is we are printing message #
+//            if (n<=0)
+//    puts "Number must be greater than 0"
+//            return
+//    end
+//            sum=0 #initializing our result as 0 #
+//            #we are adding numbers from 1 to n using for loop #
+//            for i in 1..n
+//            sum=sum+i
+//    end
+//  return sum
+//            end
+
+
+//2)
+//#function for factorial of n #
+//    def factorial(n)
+//#firstly we are checking if n is non positive, if it is we are printing message #
+//            if (n<=0)
+//    puts "Number must be greater than 0"
+//            return
+//    end
+//
+//            product=1 #initializing our result as 1(since we are multiplying 0 will not work) #
+//            #we are multiplying numbers from 1 to n(definition of factorial) using for loop #
+//            for i in 1..n
+//            product=product*i
+//    end
+//  return product
+//            end
+//
+
+//3)
+//#function for gcd #
+//    def gcd(x,y)
+//#firstly we are checking if given numbers  are non positive, if one of them is, we are printing message #
+//            if (x<=0 || y<=0)
+//    puts "Numbers must be greater
+
+
+//4)
+//#this function tells us whether the number is prime or not
+//    def isprime(n)
+//     return false if n <= 1 #cheking if n is less than 1, since 1 is not prime number
+//
+//    for i in 2..(n/2)
+//            if n % i==0
+//            return false
+//    end
+//            end
+//    return true
+//    end
+
+
+
+
+
+
+
+
 }
